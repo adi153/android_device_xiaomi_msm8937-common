97033a7149c539122e01ad2573732ff8f99cd80a
Thagoo <lohitgowda56@gmail.com>
2020-12-09 15:04:25 +0000

    Revert "init: Update build fingerprint spoof from redfin" [TEMP]
    This reverts commit 5df3b5c9f3bbda22313204bebb19b065b4447b98.

--- init/init_msm8937.cpp
+++ init/init_msm8937.cpp
@@ -157,8 +157,8 @@ void vendor_load_properties()
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", heapmaxfree);
     property_override("ro.control_privapp_permissions", "log");
-    property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.product.build.fingerprint", "google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys");
+    property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/sunfish/sunfish:11/RP1A.201005.006/6828489:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.product.build.fingerprint", "google/sunfish/sunfish:11/RP1A.201005.006/6828489:user/release-keys");
 
     
     // Workaround SafetyNet
